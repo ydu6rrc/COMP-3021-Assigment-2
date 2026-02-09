9ee680eaf722b4d2421d2e5534654be4dda2b306
yupeng-du <ydu6@rrc.ca>
2026-02-09 12:02:38 -0600

    change java again 2

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
-class sample {
-    public static void main(String[] args) {
+public class Sample {
+
+    public static void main(final String[] args) {
         System.out.println("Hello World");
     }
 }
